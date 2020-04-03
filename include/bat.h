9b5af3af75d44fb4c5080c1f9b90db9aaf7d4a12
goostaw <goostaw@debian>
2020-04-03 18:21:53 +0200

    "virtual in derived class"

--- include/bat.h
+++ include/bat.h
@@ -8,9 +8,9 @@ class Bat : public Figure
 {
 	public:
 	Bat( Map& m );
-	void iconOnMap();
-	int sprTabIndex(){ return 0; }
-	int color(){ return 2; }
+	virtual void iconOnMap();
+	virtual int sprTabIndex(){ return 0; }
+	virtual int color(){ return 2; }
 };
 
 #endif // bat_h
